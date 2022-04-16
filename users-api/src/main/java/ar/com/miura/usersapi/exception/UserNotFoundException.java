954c0073464938979c198c165cfe5fcc37647dc6
A R M <1576386-agustinmiura@users.noreply.gitlab.com>
2022-04-16 22:44:13 +0000

    Added Postgresql config and completed code.

--- users-api/src/main/java/ar/com/miura/usersapi/exception/UserNotFoundException.java
+++ users-api/src/main/java/ar/com/miura/usersapi/exception/UserNotFoundException.java
@@ -1,6 +1,9 @@
 package ar.com.miura.usersapi.exception;
 
-@org.springframework.web.bind.annotation.ResponseStatus(value = org.springframework.http.HttpStatus.NOT_FOUND)
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
---
+++ users-api/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+/*
+Insert data into the table
+*/
+INSERT INTO users."user"
+(id, creation_date, delete_time, email_address, full_name, "role", status, username)
+VALUES('id1', '2022-04-16 19:00:38.155', NULL, 'name1@gmail.com', 'full name', 'role', 'status', 'username1');
+INSERT INTO users."user"
+(id, creation_date, delete_time, email_address, full_name, "role", status, username)
+VALUES('id2', '2022-04-16 19:00:58.600', NULL, 'name2@gmail.com', 'full name', 'role', 'status', 'username2');
+INSERT INTO users."user"
+(id, creation_date, delete_time, email_address, full_name, "role", status, username)
+VALUES('id3', '2022-04-16 19:01:09.542', NULL, 'name3@gmail.com', 'full name', 'role', 'status', 'username3');
+INSERT INTO users."user"
+(id, creation_date, delete_time, email_address, full_name, "role", status, username)
+VALUES('id4', '2022-04-16 19:01:28.631', NULL, 'name4@gmail.com', 'full name4', 'role', 'status', 'username4');
+INSERT INTO users."user"
+(id, creation_date, delete_time, email_address, full_name, "role", status, username)
+VALUES('id5', '2022-04-16 19:01:45.766', NULL, 'name5@gmail.com', 'full name5', 'role', 'status', 'username5');
