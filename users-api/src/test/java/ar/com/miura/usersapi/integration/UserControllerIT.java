1de5c5935ab112efaa40598b6c3857037acc23b6
agustinmiura <miura.agustin@gmail.com>
2022-04-17 02:09:26 -0300

    Added IT tests

--- users-api/src/test/java/ar/com/miura/usersapi/integration/UserControllerIT.java
+++ users-api/src/test/java/ar/com/miura/usersapi/integration/UserControllerIT.java
@@ -1,8 +1,7 @@
 package ar.com.miura.usersapi.integration;
 
+import ar.com.miura.usersapi.dto.UserDto;
 import ar.com.miura.usersapi.utils.CustomFileUtils;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.apache.bcel.util.ClassPath;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,12 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 
 @ExtendWith(SpringExtension.class)
@@ -41,6 +42,40 @@ public class UserControllerIT {
         String expected = CustomFileUtils.readContent("json/get_one_ok.json");
         Assertions.assertEquals(expected, response);
     }
+
+    @Test
+    public void shouldCreateAnUser() throws IOException {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<String>(CustomFileUtils.readContent("json/create_user_request_ok.json"), headers);
+        UserDto result = restTemplate.postForObject("/v1/users", request, UserDto.class);
+        Assertions.assertNotNull(result.getId());
+    }
+
+    @Test
+    public void shouldDeleteAnUser() throws IOException {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<>(new String(""), headers);
+        ResponseEntity<String> response = restTemplate
+                .exchange("/v1/users/id1", HttpMethod.DELETE, request, String.class);
+        String body = response.getBody();
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+        Assertions.assertEquals(null, body);
+    }
+
+    @Test
+    public void shouldUpdateAnUser() throws IOException {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<>(CustomFileUtils.readContent("json/update_user_ok_request.json"), headers);
+        ResponseEntity<String> response = restTemplate
+                .exchange("/v1/users/id1", HttpMethod.PUT, request, String.class);
+        String body = response.getBody();
+        Assertions.assertEquals(200, response.getStatusCodeValue());
+        Assertions.assertEquals(CustomFileUtils.readContent("json/update_user_ok_response.json"), body);
+    }
+
 
 
 }
