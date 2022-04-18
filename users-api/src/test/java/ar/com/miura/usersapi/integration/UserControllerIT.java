124eaf794378d8d5a6c6c0d9c612ac396563f811
agustinmiura <miura.agustin@gmail.com>
2022-04-18 17:53:41 -0300

    Fixed integration tests

--- users-api/src/test/java/ar/com/miura/usersapi/integration/UserControllerIT.java
+++ users-api/src/test/java/ar/com/miura/usersapi/integration/UserControllerIT.java
@@ -1,5 +1,7 @@
 package ar.com.miura.usersapi.integration;
 
+import ar.com.miura.usersapi.dto.LoginInputDto;
+import ar.com.miura.usersapi.dto.LoginResponseDto;
 import ar.com.miura.usersapi.dto.UserDto;
 import ar.com.miura.usersapi.utils.CustomFileUtils;
 import org.junit.jupiter.api.Assertions;
@@ -34,15 +36,25 @@ public class UserControllerIT {
     @Test
     @WithMockUser("user")
     public void shouldGetOneUser() throws IOException {
-        String response = this.restTemplate.getForObject("/v1/users/id1", String.class);
+        LoginResponseDto loginResponseDto = login();
+        HttpHeaders headers = getHttpHeaders(loginResponseDto);
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<String> response = restTemplate.exchange("/v1/users/id1", HttpMethod.GET, entity, String.class);
+
         String expected = CustomFileUtils.readContent("json/get_one_ok.json");
-        Assertions.assertEquals(expected, response);
+        Assertions.assertEquals(expected, response.getBody());
+
     }
+
+
 
     @Test
     public void shouldCreateAnUser() throws IOException {
-        var headers = new HttpHeaders();
+        LoginResponseDto loginResponseDto = login();
+        HttpHeaders headers = getHttpHeaders(loginResponseDto);
         headers.setContentType(MediaType.APPLICATION_JSON);
+
         HttpEntity<String> request = new HttpEntity<String>(CustomFileUtils.readContent("json/create_user_request_ok.json"), headers);
         UserDto result = restTemplate.postForObject("/v1/users", request, UserDto.class);
         Assertions.assertNotNull(result.getId());
@@ -50,8 +62,11 @@ public class UserControllerIT {
 
     @Test
     public void shouldDeleteAnUser() throws IOException {
-        var headers = new HttpHeaders();
+        LoginResponseDto loginResponseDto = login();
+
+        HttpHeaders headers = getHttpHeaders(loginResponseDto);
         headers.setContentType(MediaType.APPLICATION_JSON);
+
         HttpEntity<String> request = new HttpEntity<>(new String(""), headers);
         ResponseEntity<String> response = restTemplate
                 .exchange("/v1/users/id1", HttpMethod.DELETE, request, String.class);
@@ -62,7 +77,11 @@ public class UserControllerIT {
 
     @Test
     public void shouldUpdateAnUser() throws IOException {
-        var headers = new HttpHeaders();
+        LoginResponseDto loginResponseDto = login();
+
+        HttpHeaders headers = getHttpHeaders(loginResponseDto);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(CustomFileUtils.readContent("json/update_user_ok_request.json"), headers);
         ResponseEntity<String> response = restTemplate
@@ -72,6 +91,17 @@ public class UserControllerIT {
         Assertions.assertEquals(CustomFileUtils.readContent("json/update_user_ok_response.json"), body);
     }
 
+    private LoginResponseDto login() {
+        LoginInputDto inputDto = new LoginInputDto("user", "password");
+        LoginResponseDto loginResponseDto = this.restTemplate.postForObject("/v1/authentication", inputDto, LoginResponseDto.class);
+        return loginResponseDto;
+    }
+
+    private HttpHeaders getHttpHeaders(LoginResponseDto loginResponseDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, loginResponseDto.getJwtToken());
+        return headers;
+    }
 
 
 }
