6893da4c0014a5dd0a9350dd56c9d01d1320669d
agustinmiura <agustinmiura@no-reply.github.com>
2022-09-04 19:47:30 -0300

    Added code

--- scala/first-scala-proyect/src/main/scala/com/rockthejvm/part3fp/Options.scala
+++ scala/first-scala-proyect/src/main/scala/com/rockthejvm/part3fp/Options.scala
@@ -56,4 +56,17 @@ object Options extends App {
   println(conectionStatus)
   conectionStatus.foreach(println)
 
+  config.get("host")
+    .flatMap(host => config.get("port")
+    .flatMap(port => Connection(host, port))
+    .map(connection => connection.connect))
+  .foreach(println)
+
+  val forConnectionStatus = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+  forConnectionStatus.foreach(println)
+
 }
