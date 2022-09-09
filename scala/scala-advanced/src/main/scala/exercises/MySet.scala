4125910d2c8f89d6705cc8b431e09217ca7e8566
agustinmiura <agustinmiura@no-reply.github.com>
2022-09-08 22:01:50 -0300

    Added code

--- scala/scala-advanced/src/main/scala/exercises/MySet.scala
+++ scala/scala-advanced/src/main/scala/exercises/MySet.scala
@@ -88,19 +88,3 @@ object MySet {
     buildSet(values, new EmptySet[A])
   }
 }
-
-object MySetPlayground extends App {
-  val s = MySet(1,2,3,4)
-  s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) filter (_ % 2 == 0) foreach println
-
-
-  val negative = !s
-  println(negative(2))
-  println(negative(5))
-
-  val negativeEven = negative.filter(_ % 2 == 0)
-  println(negativeEven(5))
-
-  val negativeEven5 = negativeEven + 5
-  println(negativeEven5(5))
-}
