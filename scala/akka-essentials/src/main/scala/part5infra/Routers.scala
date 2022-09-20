2157784bb2dbb7b63f2e879a7af9aef5bd6a2f7f
agustinmiura <agustinmiura@no-reply.github.com>
2022-09-19 23:54:13 -0300

    code

--- scala/akka-essentials/src/main/scala/part5infra/Routers.scala
+++ scala/akka-essentials/src/main/scala/part5infra/Routers.scala
@@ -1,7 +1,8 @@
 package part5infra
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props, Terminated}
-import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
+import akka.routing.{ActorRefRoutee, Broadcast, FromConfig, RoundRobinGroup, RoundRobinPool, RoundRobinRoutingLogic, Router}
+import com.typesafe.config.ConfigFactory
 
 object Routers extends App {
 
@@ -32,8 +33,30 @@ object Routers extends App {
 
   val system = ActorSystem("RoutersDemo")
   val master = system.actorOf(Props[Master])
+  /*
   for (i <- 1 to 10)  {
     master ! s"[$i]Hello from the world"
   }
+  */
+  val poolMaster  = system.actorOf(RoundRobinPool(5).props(Props[Slave]), "simplePoolMaster")
+
+  val slaveList = (1 to 5).map(i => system.actorOf(Props[Slave], s"slave_$i")).toList
+
+  val slavePaths = slaveList.map(slaveRef => slaveRef.path.toString)
+
+  val groupRouter = system.actorOf(RoundRobinGroup(slavePaths).props())
+  /*
+  for (i <- 1 to 10) {
+    groupRouter ! s"[$i]Hello from the world"
+  }
+  */
+
+  /*
+  val groupMaster2 = system.actorOf(FromConfig.props(), "groupMaster2")
+  for (i <- 1 to 10) {
+    groupMaster2 ! s"[$i]Hello from the world"
+  }
+  groupMaster2 ! Broadcast("hello, everyone")
+  */
 
 }
