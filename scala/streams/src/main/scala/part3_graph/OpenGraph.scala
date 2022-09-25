64e6c9af16ee77aa5bb008dd39c9ffe8da028f1c
agustinmiura <agustinmiura@no-reply.github.com>
2022-09-25 17:00:25 -0300

    Added code

---
+++ scala/streams/src/main/scala/part3_graph/OpenGraph.scala
@@ -0,0 +1,60 @@
+package part3_graph
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Broadcast, Concat, Flow, GraphDSL, Sink, Source}
+import akka.stream.{ActorMaterializer, FlowShape, SinkShape, SourceShape}
+
+object OpenGraph extends App {
+
+  implicit val system = ActorSystem("OpenGraphs")
+  implicit val materializer = ActorMaterializer()
+
+  val firstSource = Source(1 to 10)
+  val secondSource = Source(42 to 1000)
+
+  val sourceGraph = Source.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val concat = builder.add(Concat[Int](2))
+      firstSource ~> concat
+      secondSource ~> concat
+
+      SourceShape(concat.out)
+    }
+  )
+  sourceGraph.to(Sink.foreach(println)).run()
+
+  /**
+   * Complex sink
+   */
+  val sink1 = Sink.foreach[Int](x => println(s"Meaningful thing 1: $x"))
+  val sink2 = Sink.foreach[Int](x => println(s"Meaningful thing 2: $x"))
+  // step 1
+  val sinkGraph = Sink.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+      val broadcast = builder.add(Broadcast[Int](2))
+      broadcast ~> sink1
+      broadcast ~> sink2
+      SinkShape(broadcast.in)
+    }
+  )
+  firstSource.to(sinkGraph).run()
+
+  val flowIncrementer = Flow[Int].map(_ + 1)
+  val multiplier = Flow[Int].map(_ * 10)
+
+  val flowGraph = Flow.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+      val incrementerShape = builder.add(flowIncrementer)
+      val multiplierShape = builder.add(flowIncrementer)
+      incrementerShape ~> multiplierShape
+      FlowShape(incrementerShape.in, multiplierShape.out)
+    }
+  )
+
+  firstSource.via(flowGraph).to(Sink.foreach(println)).run()
+
+}
