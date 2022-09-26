3c774808f7db71594bf5a09ddb29dd13da75d3ba
agustinmiura <agustinmiura@no-reply.github.com>
2022-09-25 21:33:36 -0300

    Added code

---
+++ scala/streams/src/main/scala/part5/CustomGraphShapes.scala
@@ -0,0 +1,85 @@
+package part5
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.GraphDSL.Implicits.SourceArrow
+import akka.stream.scaladsl.{Balance, GraphDSL, Merge, RunnableGraph, Sink, Source}
+import akka.stream.{ActorMaterializer, ClosedShape, Inlet, Outlet, Shape}
+
+import scala.collection.immutable
+import scala.concurrent.duration.DurationInt
+
+object CustomGraphShapes extends App {
+
+  implicit val system = ActorSystem("CustomGraphShape")
+  implicit val materializer = ActorMaterializer()
+
+  case class Balance2x3(
+                         in0: Inlet[Int],
+                         in1: Inlet[Int],
+                         out0: Outlet[Int],
+                         out1: Outlet[Int],
+                         out2: Outlet[Int]
+                       ) extends Shape {
+
+    override val inlets: immutable.Seq[Inlet[_]] = List(in0, in1)
+    override val outlets: immutable.Seq[Outlet[_]] = List(out0, out1, out2)
+
+    override def deepCopy(): Shape = Balance2x3(
+      in0.carbonCopy(),
+      in1.carbonCopy(),
+      out0.carbonCopy(),
+      out1.carbonCopy(),
+      out2.carbonCopy()
+    )
+  }
+
+  val balanceImpl = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val merge = builder.add(Merge[Int](2))
+    val balance = builder.add(Balance[Int](3))
+
+    merge ~> balance
+
+    Balance2x3(
+      merge.in(0),
+      merge.in(1),
+      balance.out(0),
+      balance.out(1),
+      balance.out(2)
+    )
+  }
+
+  val balanceGraph = RunnableGraph.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val slowSource = Source(Stream.from(1)).throttle(1, 1 second)
+      val fastSource = Source(Stream.from(1)).throttle(1, 1 second)
+
+      def createSink(index: Int) = Sink.fold(0)((count: Int, element: Int) => {
+        println(s"[sink $index] Received $element, current count is $count")
+        count + 1
+      })
+
+      val sink1 = builder.add(createSink(1))
+      val sink2 = builder.add(createSink(2))
+      val sink3 = builder.add(createSink(3))
+
+      val balance2x3 = builder.add(balanceImpl)
+
+
+      slowSource ~> balance2x3.in0
+      fastSource ~> balance2x3.in1
+
+      balance2x3.out0 ~> sink1
+      balance2x3.out1 ~> sink2
+      balance2x3.out2 ~> sink3
+
+      ClosedShape
+    }
+  )
+
+  balanceGraph.run()
+
+}
