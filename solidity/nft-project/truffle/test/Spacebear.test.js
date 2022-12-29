bf4cfeb2b39865b5db565be58deb053df6e38d5a
agustinmiura <agustinmiura@no-reply.github.com>
2022-12-28 23:23:38 -0300

    Added code

--- solidity/nft-project/truffle/test/Spacebear.test.js
+++ solidity/nft-project/truffle/test/Spacebear.test.js
@@ -1,13 +1,11 @@
 const Spacebear = artifacts.require("SpaceBear");
+const truffleAssert = require('truffle-assertions');
 
 contract("SpaceBear", (accounts) => {
     it('should credit an NFT to a specific account', async () => {
         const spaceBear = await Spacebear.deployed();
         let txResult = await spaceBear.safeMint(accounts[1],"spacebear_1.json");
-        assert.equal(txResult.logs[0].event, "Transfer", "Transfer event was not emitted")
-        assert.equal(txResult.logs[0].args.from, '0x0000000000000000000000000000000000000000', "Token was not transferred from the zero address");
-        assert.equal(txResult.logs[0].args.to, accounts[1], "Receiver wrong address");
-        assert.equal(txResult.logs[0].args.tokenId.toString(), "0", "Wrong Token ID minted");
+        truffleAssert.eventEmitted(txResult, 'Transfer', {from: '0x0000000000000000000000000000000000000000', to: accounts[1], tokenId: web3.utils.toBN("0")});
         assert.equal(await spaceBear.ownerOf(0), accounts[1], "NFT was not credited to the correct account")
     });
 });
