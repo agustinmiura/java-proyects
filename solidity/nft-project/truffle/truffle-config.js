6839baa608e42a73d67e88ecb2a9adf6412d5c17
agustinmiura <agustinmiura@no-reply.github.com>
2022-12-28 23:56:47 -0300

    Added code

--- solidity/nft-project/truffle/truffle-config.js
+++ solidity/nft-project/truffle/truffle-config.js
@@ -1,50 +1,7 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation, and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * https://trufflesuite.com/docs/truffle/reference/configuration
- *
- * Hands-off deployment with Infura
- * --------------------------------
- *
- * Do you have a complex application that requires lots of transactions to deploy?
- * Use this approach to make deployment a breeze 🏖️:
- *
- * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
- * to sign transactions before they're sent to a remote public node.
- * Infura accounts are available for free at 🔍: https://infura.io/register
- *
- * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. You can store your secrets 🤐 in a .env file.
- * In your project root, run `$ npm install dotenv`.
- * Create .env (which should be .gitignored) and declare your MNEMONIC
- * and Infura PROJECT_ID variables inside.
- * For example, your .env file will have the following structure:
- *
- * MNEMONIC = <Your 12 phrase mnemonic>
- * PROJECT_ID = <Your Infura project id>
- *
- * Deployment with Truffle Dashboard (Recommended for best security practice)
- * --------------------------------------------------------------------------
- *
- * Are you concerned about security and minimizing rekt status 🤔?
- * Use this method for best security:
- *
- * Truffle Dashboard lets you review transactions in detail, and leverages
- * MetaMask for signing, so there's no need to copy-paste your mnemonic.
- * More details can be found at 🔎:
- *
- * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
- */
-
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require('fs');
+const mnemonicPhrase = fs.readFileSync(".secret").toString().trim();
+const infuraProjectID = fs.readFileSync(".infura").toString().trim();
 
 module.exports = {
   networks: {
@@ -52,7 +9,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    }
+    },
+    goerli: {
+      provider: () =>
+          new HDWalletProvider({
+          mnemonic: {
+              phrase: mnemonicPhrase
+          },
+          providerOrUrl: "https://goerli.infura.io/v3/" + infuraProjectID
+          }),
+      network_id: '5',
+      }
   },
 
   mocha: {
@@ -62,6 +29,24 @@ module.exports = {
     solc: {
       version: "0.8.17" // Fetch exact version from solc-bin (default: truffle's version)
     }
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.16",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+      //  evmVersion: "byzantium"
+      }
+    }
+  },
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: fs.readFileSync('.etherscan').toString().trim()
   }
 
 };
