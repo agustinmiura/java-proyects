7ca2910a02e3f8b049eb24e0c3cbfc7cc7fbf383
agustinmiura <agustinmiura@no-reply.github.com>
2022-12-29 22:00:16 -0300

    Added code

--- solidity/tokenization/truffle/contracts/MyToken.sol
+++ solidity/tokenization/truffle/contracts/MyToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("Starducks Capu-token", "SCT") {
-        _mint(msg.sender, 100 * 10 ** decimals());
+    constructor(uint256 initialSupply) ERC20("Starducks Capu-token", "SCT") {
+        _mint(msg.sender, initialSupply);
     }
 }
